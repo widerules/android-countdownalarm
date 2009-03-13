9478924dbbdb6b647b68e7fd50667a3feaf8412b
adam <adam@everythingandroid.net>
2009-03-13 23:46:42 +0000

    v1.0.8

--- CountdownTimer/src/net/everythingandroid/timer/Log.java
+++ CountdownTimer/src/net/everythingandroid/timer/Log.java
@@ -1,12 +1,14 @@
 package net.everythingandroid.timer;
 
+import android.util.Config;
+
 
 public class Log {
     public final static String LOGTAG = "Countdown Alarm++";
 
-    private static final boolean DEBUG = false;
-    //static final boolean LOGV = DEBUG ? Config.LOGD : Config.LOGV;
-    static final boolean LOGV = true;
+ 	private static final boolean DEBUG = true;
+	// private static final boolean DEBUG = false;
+	static final boolean LOGV = DEBUG ? Config.LOGD : Config.LOGV;
 
     static void v(String msg) {
     	if (LOGV) {
